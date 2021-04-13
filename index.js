563fbefd3f3fe378d9d0e35c31d71850f3e045b1
fitzgebt <fitzgebt@gmail.com>
2021-04-13 13:35:36 -0700

    commit tree 6d4e4c1266b91ad039aba4f05caca93c0cf72cc1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let completedCards = []
+    for (let i=0; i < names.length; i++) {
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return completedCards
+}
+
+function countDown(x) {
+    while (x > 0) {
+        console.log(x);
+        x -= 1;
+    }
+    console.log(x);
+}
